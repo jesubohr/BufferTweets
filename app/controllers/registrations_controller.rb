98cb20e354e9f92bffc1780cf43825f24677a013
Jesus Borrero <jesufer2018@hotmail.com>
2023-04-02 01:49:17 -0500

    Add user signup & auto-login

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,6 +5,21 @@ class RegistrationsController < ApplicationController
   end
 
   def create
-    render plain: params[:user], status: :ok
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:alert_title] = 'Thank you for signing up!'
+      flash[:alert_message] = 'You are now signed up and logged in.'
+      flash[:alert_type] = 'success'
+      redirect_to root_path
+    else
+      render :new, status: 400
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
