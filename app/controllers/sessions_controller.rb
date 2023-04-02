2adb99a840b59b7b7bd9bab044d0654c7a85436d
Jesus Borrero <jesufer2018@hotmail.com>
2023-04-02 02:40:11 -0500

    Add sign-in route & controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,49 @@
+class SessionsController < ApplicationController
+  def new
+    user = User.find_by(id: session[:user_id]) if session[:user_id]
+    return unless user.present?
+
+    redirect_to root_path, flash: {
+      alert_title: 'You are already logged in',
+      alert_message: 'You can log out below',
+      alert_type: 'info'
+    }
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      logged_in
+    else
+      not_logged_in
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, flash: {
+      alert_title: 'You have been logged out',
+      alert_message: 'Come back soon!',
+      alert_type: 'info'
+    }
+  end
+
+  private
+
+  def logged_in
+    redirect_to root_path, flash: {
+      alert_title: 'You have been logged in',
+      alert_message: 'Welcome back!',
+      alert_type: 'success'
+    }
+  end
+
+  def not_logged_in
+    redirect_to sign_in_path, flash: {
+      alert_title: 'Invalid email or password',
+      alert_message: 'Please try again',
+      alert_type: 'error'
+    }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   get 'about', to: 'about#index'
+
   get 'sign-up', to: 'registrations#new', as: :sign_up
   post 'sign-up', to: 'registrations#create'
+
+  get 'sign-in', to: 'sessions#new', as: :sign_in
+  post 'sign-in', to: 'sessions#create'
+
+  delete 'logout', to: 'sessions#destroy'
   root to: 'main#index'
 end
