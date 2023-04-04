794fded0d5559083ff3ae31cf0ef54f35a34856b
Jesus Borrero <jesufer2018@hotmail.com>
2023-04-04 16:32:57 -0500

    Add model-level validation to password fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,11 @@ class User < ApplicationRecord
     with: /\A[^@\s]+@[^@\s]+\z/,
     message: 'Must be a valid email address'
   }
+
+  validates :password, presence: true, length: { minimum: 8, maximum: 32 }, format: {
+    with: /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,32}\z/,
+    message: 'Must contain at least one uppercase letter, one lowercase letter, and one number'
+  }
+
+  validates :password_confirmation, presence: true
 end
